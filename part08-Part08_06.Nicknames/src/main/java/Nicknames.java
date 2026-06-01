569239a4d0baeb78da9a0c1dfb6ec6416d7fbed8
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-01 18:23:48 +0300

    Learning HashMaps

--- part08-Part08_06.Nicknames/src/main/java/Nicknames.java
+++ part08-Part08_06.Nicknames/src/main/java/Nicknames.java
@@ -4,7 +4,12 @@ import java.util.HashMap;
 public class Nicknames {
 
     public static void main(String[] args) {
-        // Do the operations required here!
+        HashMap<String,String> hash = new HashMap<>();
+        hash.put("matthew", "matt");
+        hash.put("michael", "mix");
+        hash.put("arthur", "artie");
+        
+        System.out.println(hash.get("matthew"));
     }
 
 }
