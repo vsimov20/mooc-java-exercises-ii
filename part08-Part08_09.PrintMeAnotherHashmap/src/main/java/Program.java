feff0324a82c49f4d165a355032bba2b9150f6df
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-02 19:22:33 +0300

    HashMap values()

--- part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -4,7 +4,28 @@ import java.util.HashMap;
 public class Program {
 
     public static void main(String[] args) {
-        // Test your program here!
+        HashMap<String,Book> hashmap = new HashMap<>();
+        
+        hashmap.put("prejudice", new Book("Pride and prejudice", 1813, "...."));
+        hashmap.put("sense", new Book("Sense and Sensibility", 1811, "..."));
+        
+        printValues(hashmap);
+        System.out.println("---");
+        printValueIfNameContains(hashmap, "Sen");
+    }
+    
+    public static void printValues(HashMap<String,Book> hashmap){
+        for(Book book: hashmap.values()){
+            System.out.println(book);
+        }
+    }
+    
+    public static void printValueIfNameContains(HashMap<String,Book> hashmap, String title){
+        for(Book book: hashmap.values()){
+            if(book.getName().contains(title)){
+                System.out.println(book);
+            }
+        }
     }
 
 }
