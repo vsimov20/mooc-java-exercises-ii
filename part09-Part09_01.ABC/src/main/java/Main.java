538af24e8ec129ce0455ac11a5bc3d709a78e7b5
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-06 17:19:43 +0300

    Intorduction to Inheretance

--- part09-Part09_01.ABC/src/main/java/Main.java
+++ part09-Part09_01.ABC/src/main/java/Main.java
@@ -2,7 +2,12 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write your test code here
+
+        C c = new C();
+        
+        c.a();
+        c.b();
+        c.c();
         
     }
 }
