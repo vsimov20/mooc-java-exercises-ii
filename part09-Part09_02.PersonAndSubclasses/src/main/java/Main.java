86665f76bfa2d3fa0439c76aac1afa5159c21882
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-06 17:56:25 +0300

    Working with sub- and superclasses

--- part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Main.java
@@ -1,8 +1,32 @@
+import java.util.ArrayList;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your test code here
+        ArrayList<Person> persons = new ArrayList<>();
+        
+        Teacher ada = new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200);
+        Teacher esko = new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki", 5400);
+        persons.add(ada);
+        persons.add(esko);
+
+        Student ollie = new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028");
+
+        int i = 0;
+        while (i < 25) {
+            ollie.study();
+            i = i + 1;
+        }
+        
+        persons.add(ollie);
+        
+        printPersons(persons);
+    }
+    
+    public static void printPersons(ArrayList<Person> persons){
+        for(Person person: persons){
+            System.out.println(person);
+        }
     }
 
 }
---
+++ part09-Part09_02.PersonAndSubclasses/src/main/java/Student.java
@@ -0,0 +1,23 @@
+
+public class Student extends Person {
+    private int credits;
+    
+    public Student(String name, String adress) {
+        super(name, adress);
+        this.credits = 0;
+    }
+    
+    public int credits(){
+        return this.credits;
+    }
+    
+    public void study(){
+        this.credits++;
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString() + "\n" + "  Study credits " + this.credits;
+    }
+    
+}
