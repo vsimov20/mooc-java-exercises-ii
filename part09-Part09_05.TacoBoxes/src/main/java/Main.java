ec023a8e453ec2868056545d8ad72d6e7694f828
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-10 17:11:47 +0300

    Introduction to interfaces

--- part09-Part09_05.TacoBoxes/src/main/java/Main.java
+++ part09-Part09_05.TacoBoxes/src/main/java/Main.java
@@ -2,6 +2,16 @@
 public class Main {
 
     public static void main(String[] args) {
-        // Test your code here!
+        TripleTacoBox tacos = new TripleTacoBox();
+        
+        System.out.println(tacos.tacosRemaining());
+        
+        tacos.eat();
+        tacos.eat();
+        tacos.eat();
+        tacos.eat();
+        tacos.eat();
+        
+        System.out.println(tacos.tacosRemaining());
     }
 }
