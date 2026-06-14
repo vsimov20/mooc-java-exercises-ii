4d2db768685080abf83c4c096fec34a2e52b145b
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-14 19:39:04 +0300

    Abstract classes and interfaces

--- part09-Part09_12.Animals/src/main/java/Main.java
+++ part09-Part09_12.Animals/src/main/java/Main.java
@@ -2,7 +2,15 @@
 public class Main {
 
     public static void main(String[] args) {
-        // you can test how your classes work here
+        Dog dog = new Dog();
+        dog.eat();
+        dog.sleep();
+        dog.makeNoise();
+        
+        Cat cat = new Cat();
+        cat.eat();
+        cat.sleep();
+        cat.makeNoise();
 
     }
 
