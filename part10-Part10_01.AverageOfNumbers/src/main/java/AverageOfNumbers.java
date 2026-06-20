5fc5bb0376c1a2f8c2bb91aaa324794c3f8b41f5
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-20 18:32:52 +0300

    Intorduction to streams

--- part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ part10-Part10_01.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -6,7 +6,26 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // Write your program here
+        ArrayList<String> nums = new ArrayList<>();
+        
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while(true){
+            String input = scanner.nextLine();
+            
+            if(input.equals("end")){
+                break;
+            }
+            
+            nums.add(input);
+        }
+        
+        double avgNums = nums.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .average()
+                .getAsDouble();
+        
+        System.out.println("average of the numbers: " + avgNums);
+                
 
     }
 }
