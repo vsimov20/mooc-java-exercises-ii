89159b7a2c4b913b1f50ee53bb7f7d508585a4aa
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-20 19:38:15 +0300

    streams with filter() method

--- part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
+++ part10-Part10_02.AverageOfSelectedNumbers/src/main/java/AverageOfSelectedNumbers.java
@@ -6,7 +6,44 @@ public class AverageOfSelectedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        ArrayList<String> nums = new ArrayList<>();
+        
+        System.out.println("Input numbers, type \"end\" to stop.");
+        while(true){
+            String input = scanner.nextLine();
+            
+            if(input.equals("end")){
+                break;
+            }
+            
+            nums.add(input);
+        }
+        
+        System.out.println("Print the average of the negative numbers or the positive numbers? (n/p)");
+        String input = scanner.nextLine();
+        
+        if(input.equals("n")){
+            double avgNegative = nums.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .filter(num -> num < 0)
+                .average()
+                .getAsDouble();
+            
+            System.out.println("Average of the negative numbers: " + avgNegative);
+        }
+        
+        if(input.equals("p")){
+            double avgPositive = nums.stream()
+                .mapToInt(s -> Integer.valueOf(s))
+                .filter(num -> num > 0)
+                .average()
+                .getAsDouble();
+            
+            System.out.println("Average of the positive numbers: " + avgPositive);
+        }
+        
+        
+        
 
     }
 }
