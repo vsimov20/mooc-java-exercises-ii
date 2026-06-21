e055592c349d64662a49945c78765343bdca6946
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-21 19:06:38 +0300

    filtering streams of lists

--- part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
+++ part10-Part10_03.PositiveNumbers/src/main/java/PositiveNumbers.java
@@ -2,13 +2,31 @@
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import java.util.ArrayList;
 
 public class PositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // test your method here
+        ArrayList<Integer> nums = new ArrayList<>();
+        nums.add(4);
+        nums.add(3);
+        nums.add(-4);
+        nums.add(16);
+        nums.add(-15);
+        nums.add(0);
+        nums.add(5);
+        
+        System.out.println(positive(nums));
 
+    }
+    
+    public static List<Integer> positive(List<Integer> numbers){
+        ArrayList<Integer> nums = numbers.stream()
+                .filter(value -> value > 0)
+                .collect(Collectors.toCollection(ArrayList::new));
+        return nums;
+        
     }
 
 }
