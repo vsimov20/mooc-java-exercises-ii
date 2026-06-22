52ec07ec92c2911027f52103daeaa67a724bc20e
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-22 18:08:29 +0300

    .forEach()

--- part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
+++ part10-Part10_05.PrintingUserInput/src/main/java/PrintingUserInput.java
@@ -7,5 +7,19 @@ public class PrintingUserInput {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        ArrayList<String> str = new ArrayList<>();
+
+        while (true) {
+            String input = scanner.nextLine();
+            if(input.equals("")){
+                break;
+            }
+            
+            str.add(input);
+        }
+        
+        str.stream()
+                .forEach(s -> System.out.println(s));
+        
     }
 }
