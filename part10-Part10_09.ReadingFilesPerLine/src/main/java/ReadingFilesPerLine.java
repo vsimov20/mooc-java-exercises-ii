1e92eaa69e1276ad3c8006f5f9c0fda617a5e943
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-23 19:07:05 +0300

    reading files using streams

--- part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
+++ part10-Part10_09.ReadingFilesPerLine/src/main/java/ReadingFilesPerLine.java
@@ -10,8 +10,23 @@ public class ReadingFilesPerLine {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        // test the method here
+        System.out.println(read("habits_tracker.txt"));
 
+    }
+    
+    public static List<String> read(String file){
+        List<String> rows = new ArrayList<>();
+        
+        try{
+            Files.lines(Paths.get(file))
+                    .forEach(row -> rows.add(row));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            
+        }
+        
+        return rows;
+                
     }
 
 }
