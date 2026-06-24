f76de46d5b7440ad83724ac435bc57e55d246d94
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-24 17:51:34 +0300

    Introduction to the Comparable interface

--- part10-Part10_11.WageOrder/src/main/java/MainProgram.java
+++ part10-Part10_11.WageOrder/src/main/java/MainProgram.java
@@ -6,16 +6,13 @@ public class MainProgram {
 
     public static void main(String[] args) {
         ArrayList<Human> humans = new ArrayList<>();
-        humans.add(new Human("Matti", 150000));
-        humans.add(new Human("Merja", 500));
+        humans.add(new Human("Matti", 150));
+        humans.add(new Human("Merja", 5000));
         humans.add(new Human("Pertti", 80));
 
         System.out.println(humans);
 
-        /*
-         * Uncomment the comment below when you have completed the compareTo-method.
-         */
-        //Collections.sort(humans);
+        Collections.sort(humans);
         System.out.println(humans);
 
     }
