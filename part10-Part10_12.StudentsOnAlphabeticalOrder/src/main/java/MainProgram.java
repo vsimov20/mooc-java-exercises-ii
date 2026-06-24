9662286f18388057a7590667bd6961ff2ac45461
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-24 18:05:13 +0300

    using compareTo() with Strings

--- part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/MainProgram.java
+++ part10-Part10_12.StudentsOnAlphabeticalOrder/src/main/java/MainProgram.java
@@ -1,10 +1,21 @@
-
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class MainProgram {
 
     public static void main(String[] args) {
-//        Student first = new Student("jamo");
-//        Student second = new Student("jamo1");
-//        System.out.println(first.compareTo(second));
+        Student first = new Student("jamo");
+        Student second = new Student("sumo");
+        Student third = new Student("limo");
+        
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(first);
+        students.add(second);
+        students.add(third);
+        
+        System.out.println(students);
+        
+        Collections.sort(students);
+        System.out.println(students);
     }
 }
