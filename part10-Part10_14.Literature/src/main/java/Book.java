605f5c7724d124df1498dcf0fc36c6d128419e71
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-06-25 18:07:10 +0300

    Sorting by multiple criteria

---
+++ part10-Part10_14.Literature/src/main/java/Book.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class Book {
+    private String name;
+    private int age;
+    
+    public Book(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public int getAge(){
+        return this.age;
+    }
+    
+    @Override
+    public String toString(){
+        return this.name + " (recommended for " + this.age + " year-olds or older)";
+    }
+}
--- part10-Part10_14.Literature/src/main/java/MainProgram.java
+++ part10-Part10_14.Literature/src/main/java/MainProgram.java
@@ -1,10 +1,41 @@
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Collections;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Book> books = new ArrayList<>();
+        
+        while(true){
+            System.out.print("Input the name of the book, empty stops: ");
+            String name = scanner.nextLine();
+            
+            if(name.equals("")){
+                break;
+            }
+            
+            System.out.print("Input the age recommendation: ");
+            int age = Integer.valueOf(scanner.nextLine());
+            
+            books.add(new Book(name, age));
+        }
+        System.out.println("");
+        System.out.println(books.size() + " books in total.\n");
+        
+        Comparator<Book> comparator = Comparator
+                .comparing(Book::getAge)
+                .thenComparing(Book::getName);
+        
+        Collections.sort(books, comparator);
+        
+        System.out.println("Books:");
+        
+        books.stream()
+                .forEach(book -> System.out.println(book));
 
     }
 
