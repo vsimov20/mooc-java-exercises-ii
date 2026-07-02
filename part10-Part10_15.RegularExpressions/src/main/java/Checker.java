46caa86e2dacbcc15eac7367322def7791de67dd
vsimov20 <125960912+vsimov20@users.noreply.github.com>
2026-07-02 18:24:42 +0300

    introduction to regex

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,26 @@
 
 
 public class Checker {
+    
+    public boolean isDayOfWeek(String str){
+        if(str.matches("mon|tue|wed|thu|fri|sat|sun")){
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean allVowels(String str){
+        if(str.matches("[aeiou]*")){
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean timeOfDay(String str){
+        if(str.matches("([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]")){
+            return true;
+        }
+        return false;
+    }
 
 }
